edcd3853c8abb1382906c0d87b2b8fa1a00f0f40
priyankacute <bhavanimurugesh2000@gmail.com>
2023-07-18 14:32:13 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,10 @@
+var n=prompt("enter the n value");
+for(let i=1;i<=n;i++)
+					{
+					for(let j=1;j<=i;j++)
+						{
+						document.write("*");
+						document.write(" ");
+						}
+						document.write("<br>");
+					}	
